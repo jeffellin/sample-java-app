d2f9c05f706c14eee8487c6286b9ec30396a46be
Jeffrey Ellin <11721615+jeffellin@users.noreply.github.com>
2021-10-27 15:53:03 -0400

    Update SampleApplication.java

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		//hi
+		//hi2
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
